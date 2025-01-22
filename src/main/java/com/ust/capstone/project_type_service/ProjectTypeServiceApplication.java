61695b8346632960fa0d51fe998a22a6e91baf90
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-22 23:14:31 +0530

    initial upload

---
+++ src/main/java/com/ust/capstone/project_type_service/ProjectTypeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_type_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTypeServiceApplication.class, args);
+	}
+
+}
